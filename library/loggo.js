b89f66f05f9ec636097fe00c5fc4ccbbb0e5a30c
chee <chee@snoot.club>
2018-12-22 16:20:29 +0000

    add more loggy woggles

--- library/loggo.js
+++ library/loggo.js
@@ -1,41 +1,33 @@
 let chalk = require("chalk")
 let random = array => array[array.length * Math.random() | 0]
 
-let logColors = [
-	"#ff00ff",
-	"#66ffcc",
-	"#3399ff",
-	"#f7f7f7",
-	"#feeefe",
-	"#abeabe",
-	"#aabbee"
-]
+let colors = {
+	log: [
+		"#ff00ff",
+		"#66ffcc",
+		"#3399ff",
+		"#abeabe",
+		"#aabbee"
+	],
+	warn: [
+		"#eeaa55",
+		"#cccc00",
+		"#cccc44",
+		"#cc9911",
+		"#ccaa00"
+	],
+	shout: [
+		"#ff6677",
+		"#ff2a50",
+		"#ff6666",
+		"#ff0000",
+		"#cc3366"
+	]
+}
 
-let warnColors = [
-	"#ffff00",
-	"#ffee33",
-	"#f7f344"
-]
-
-let shoutColors = [
-	"#ff6677",
-	"#ff2a50",
-	"#ff6666",
-	"#ff0000",
-	"#cc3366"
-]
-
-exports.log = message => (
-	console.log(chalk.hex(random(logColors)).bold(message)),
-	message
-)
-
-exports.warn = message => (
-	console.warn(chalk.hex(random(warnColors))(message)),
-	message
-)
-
-exports.shout = message => (
-	console.error(chalk.hex(random(shoutColors)).bold(message)),
-	message
-)
+Object.entries(colors).forEach(([level, colors]) => {
+	exports[level] = message => (
+		console.log(chalk.hex(random(colors)).bold(message)),
+		message
+	)
+})
