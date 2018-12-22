1177aca85d52dcc96b7507b28bc4491f5ce6d4e2
chee <chee@snoot.club>
2018-12-22 15:20:10 +0000

    include proxy cache and expires in skeleton nginx config

--- library/skeletons.js
+++ library/skeletons.js
@@ -28,6 +28,8 @@ exports.files = {
 		proxy_set_header X-Real-IP $remote_addr;
 		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
 		proxy_set_header X-Forwarded-Proto $scheme;
+		include /www/snoot.club/blocks/proxy-cache.nginx;
+		expires $expires;
 		client_max_body_size 222m;
 	}
 }
